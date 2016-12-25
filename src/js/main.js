5359cc7cc712c03f7a6cd7630b734701af1154ec
makedonsky1111 <maliarchuk89@gmail.com>
2016-12-25 15:49:33 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -56,6 +56,46 @@ $(document).ready(function() {
         ".": ".",
         "/": "/",
         "space": " ",
+        "tr": "`",
+        "td": "~",
+        "!": "!",
+        "@": "@",
+        "#": "#",
+        "$": "$",
+        "%": "%",
+        "^": "^",
+        "&": "&",
+        "*": "*",
+        "(": "(",
+        ")": ")",
+        "_": "_",
+        "+": "+",
+        "Q": "Q",
+        "W": "W",
+        "E": "E",
+        "R": "R",
+        "T": "T",
+        "Y": "Y",
+        "U": "U",
+        "I": "I",
+        "O": "O",
+        "P": "P",
+        "A": "A",
+        "S": "S",
+        "D": "D",
+        "F": "F",
+        "G": "G",
+        "H": "H",
+        "J": "J",
+        "K": "K",
+        "L": "L",
+        "Z": "Z",
+        "X": "X",
+        "C": "C",
+        "V": "V",
+        "B": "B",
+        "N": "N",
+        "M": "M",
 
         delet: function() { $('#scrin').val(''); },
         
@@ -67,11 +107,31 @@ $(document).ready(function() {
     
   // 2.Функція  
 $.fn.keyPresser = function() {
-    this.on( "click", function() {  
-    pressedKey = $(this).data('value');
-    $('#scrin').append(keysObj[pressedKey]);
-    return this;
+    
+    $("#caps").on( "click", function() {
+        $(this).toggleClass( "caps-on" ); //включає\виключає капс
     });
+    
+    
+    this.on( "click", function() {    
+        if(!$("#caps").hasClass("caps-on")){ //якщо класу нема то бере дані з обєкта по велью
+        pressedKey = $(this).data('value');
+        $('#scrin').append(keysObj[pressedKey]);
+        return this;
+        }
+    });
+    
+    this.on( "click", function() {    
+        if($("#caps").hasClass("caps-on")){  // якшо є то по велью1
+        pressedKey = $(this).data('value1');
+        $('#scrin').append(keysObj[pressedKey]);
+        return this;
+        }
+    });
+    
+    
+  
+    
 };
     
 $("button").keyPresser();  
